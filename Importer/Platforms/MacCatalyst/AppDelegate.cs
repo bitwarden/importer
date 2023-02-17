e76d9e17bc364ff7d2c2ee2f83817e92ee36fb5c
Kyle Spearrin <kyle.spearrin@gmail.com>
2023-02-17 08:59:48 -0500

    revert window sizing for mac

--- Importer/Platforms/MacCatalyst/AppDelegate.cs
+++ Importer/Platforms/MacCatalyst/AppDelegate.cs
@@ -5,31 +5,5 @@ namespace Bit.Importer;
 [Register("AppDelegate")]
 public class AppDelegate : MauiUIApplicationDelegate
 {
-    protected override MauiApp CreateMauiApp()
-    {
-        var app = MauiProgram.CreateMauiApp();
-        SetMainWindowStartSize(650, 1200);
-        return app;
-    }
-
-    private void SetMainWindowStartSize(int width, int height)
-    {
-        Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(
-            nameof(IWindow), (handler, view) =>
-            {
-                var size = new CoreGraphics.CGSize(width, height);
-                handler.PlatformView.WindowScene.SizeRestrictions.MinimumSize = size;
-                handler.PlatformView.WindowScene.SizeRestrictions.MaximumSize = size;
-                Task.Run(() =>
-                {
-                    Thread.Sleep(1000);
-                    MainThread.BeginInvokeOnMainThread(() =>
-                    {
-                        handler.PlatformView.WindowScene.SizeRestrictions.MinimumSize = new CoreGraphics.CGSize(100, 100);
-                        handler.PlatformView.WindowScene.SizeRestrictions.MaximumSize = new CoreGraphics.CGSize(5000, 5000);
-                    });
-                });
-
-            });
-    }
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
