c8ad33b69bb59911e659c201c0e82df9d0edf308
Kyle Spearrin <kyle.spearrin@gmail.com>
2023-05-16 14:58:32 -0400

    hack to make window sizing work for macos

--- Importer/App.xaml.cs
+++ Importer/App.xaml.cs
@@ -17,6 +17,22 @@ public partial class App : Application
         window.Width = 650;
         window.Height = 1150;
         window.Title = "Bitwarden Importer";
+
+        if (DeviceInfo.Platform != DevicePlatform.WinUI)
+        {
+            window.MinimumWidth = 650;
+            window.MinimumHeight = 1150;
+
+            Dispatcher.DispatchAsync(async () =>
+            {
+                await Task.Delay(1000);
+                window.MinimumWidth = 0;
+                window.MinimumHeight = 0;
+                window.MaximumWidth = double.PositiveInfinity;
+                window.MaximumHeight = double.PositiveInfinity;
+            });
+        }
+
         return window;
     }
 }
